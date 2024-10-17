38cb6635a18a55892386af566aeadeced86e8997
giuliopedicone02 <pediconegiulio02@gmail.com>
2024-10-17 11:49:10 +0200

    Add 2024-10-17

---
+++ Esercizi/2024-10-17/server.php
@@ -0,0 +1,7 @@
+<?php
+echo '<b> $_SERVER[PHP_SELF]: </b>' . $_SERVER['PHP_SELF'] . "<br>";
+echo '<b> $_SERVER[SERVER_NAME]: </b>' . $_SERVER['SERVER_NAME'] . "<br>";
+echo '<b> $_SERVER[HTTP_HOST]: </b>' . $_SERVER['HTTP_HOST'] . "<br>";
+echo '<b>$_SERVER[HTTP_REFERER]: </b>' . $_SERVER['HTTP_REFERER'] . "<br>";
+echo '<b> $_SERVER[HTTP_USER_AGENT]: </b>' . $_SERVER['HTTP_USER_AGENT'] . "<br>";
+echo '<b> $_SERVER[SCRIPT_NAME]: </b>' . $_SERVER['SCRIPT_NAME'];
